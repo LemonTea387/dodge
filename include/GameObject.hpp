259172caa637d8aaf60b90a6f10c49a47c8bbd50
LemonTea387 <JunJieLow08@gmail.com>
2023-09-29 01:01:49 +0800

    Object Transforms

--- include/GameObject.hpp
+++ include/GameObject.hpp
@@ -10,4 +10,13 @@ class Drawable {
   virtual void Render(RenderWindow &renderWindow) = 0;
 };
 
+struct Transform {
+  int x;
+  int y;
+};
+class Transformable {
+protected:
+  Transform GetTransform() { return transform; };
+  Transform transform;
+};
 #endif
